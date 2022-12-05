e3473416fc5fe31bb0c6ff79008f18f50248171c
grbalmeida <g.r.almeida@live.com>
2022-12-04 23:02:19 -0300

    Exibindo dados dos banco

--- receitas/views.py
+++ receitas/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render
+from .models import Receita
 
 def index(request):
     
-    receitas = {
-        1: 'Lasanha',
-        2: 'Sopa de legumes',
-        3: 'Sorvete',
-        4: 'Bolo de chocolate'
-    }
+    receitas = Receita.objects.all()
     
     dados = {
-        'nome_das_receitas': receitas
+        'receitas': receitas
     }
     
     return render(request, 'index.html', dados)
